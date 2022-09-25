534d8addeb9ae5068969d5f109e2a9d3f6676b2b
hybeesalau <106175952+hybeesalau@users.noreply.github.com>
2022-09-24 23:14:19 -0500

    Add files via upload

---
+++ f_one.py
@@ -0,0 +1,12 @@
+# # username = input("Enter username:\n")
+# # password = input("Enter Password:\n")
+# # if username == "admin" or password == "admin123":
+# #     print("valid user")
+# # else:
+# #     print("invalid user")
+# #
+#
+# counter = 1
+# while  counter < 11:
+#     print(counter)
+#     counter += 1
